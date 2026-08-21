5e82f0512bd21a55a26809ca484a876f2929c5c8
Andisyc <andisyc@163.com>
2026-08-21 15:28:07 +0800

    threshold testing

--- run/run_frontres_clean_calibration_v010.sh
+++ run/run_frontres_clean_calibration_v010.sh
@@ -10,6 +10,9 @@ CACHE="${CACHE_DIR:-/hdd1/cyx/AMASS_G1Segment}"
 MANIFEST="${CLEAN_CALIBRATION_MANIFEST:-${ROOT}/note/testing/manifests/frontres_clean_calibration_v010_server.json}"
 FUTURE_OFFSETS="${FRONTRES_V015_FUTURE_OFFSETS:-1,2}"
 K_CURRICULUM="${FRONTRES_V015_K_CURRICULUM:-8:4:200:500:1300:lower-k8:0.5:linear-coupled-v1:700:2.381,16:4:300:300:900:lower-k16:0.6:linear-coupled-v1:600:2.381,32:4:400:300:625:lower-k32:0.7:linear-coupled-v1:700:2.381}"
+ACTOR_LR_INIT="${FRONTRES_SEGMENT_ACTOR_LR_INIT:-3e-7}"
+ACTOR_LR="${FRONTRES_SEGMENT_ACTOR_LR:-1e-6}"
+CRITIC_LR="${FRONTRES_SEGMENT_CRITIC_LR:-1e-5}"
 TAG="${RUN_TAG:-20260821}"
 RESULT="${CLEAN_CALIBRATION_RESULT:-${ROOT}/FRS_EVAL_V010_CLEAN_CALIBRATION_R1_${TAG}.json}"
 LOG="${CLEAN_CALIBRATION_LOG:-${ROOT}/FRS_EVAL_V010_CLEAN_CALIBRATION_R1_${TAG}.log}"
@@ -33,8 +36,12 @@ nohup "${PYTHON_BIN}" scripts/rsl_rl/train.py \
   --frontres_stage=stage3_segment_hrl \
   --frontres_specialist_mode=rp \
   --frontres_segment_cache_dir="${CACHE}" \
+  --frontres_segment_ppo_schedule=fixed \
   --frontres_v015_future_offsets="${FUTURE_OFFSETS}" \
   --frontres_segment_k_curriculum="${K_CURRICULUM}" \
+  --frontres_segment_actor_lr_init="${ACTOR_LR_INIT}" \
+  --frontres_segment_actor_lr="${ACTOR_LR}" \
+  --frontres_segment_critic_lr="${CRITIC_LR}" \
   --frontres_v015_hsl_initializer_checkpoint="${HSL}" \
   --frontres_clean_calibration_collect_only \
   --frontres_clean_calibration_manifest="${MANIFEST}" \
